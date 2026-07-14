8bf491614fa113132a2f050c5b2dc49cfb84196f
fahdaguenouz <faguenouz@gmail.com>
2026-07-14 05:50:59 -0400

    remove commat

--- set-internal-vars.sh
+++ set-internal-vars.sh
@@ -6,7 +6,7 @@ MY_NUM=100
 MY_PI=3.142
 MY_ARR=("one" "two" "three" "four" "five")
 
-echo "$MY_MESSAGE"
-echo "$MY_NUM"
-echo "$MY_PI"
-echo "${MY_ARR[*]}"
+echo $MY_MESSAGE
+echo $MY_NUM
+echo $MY_PI
+echo ${MY_ARR[*]}
