7835e58d983ab28266b8379e580337607f790c0c
fahdaguenouz <faguenouz@gmail.com>
2026-07-15 05:57:36 -0400

    append output

--- append-output.sh
+++ append-output.sh
@@ -1,2 +1,8 @@
+cat <<EOF >results.txt
+"In the End" - Linkin Park
+"Crawling" - Linkin Park
+"Elevation" - U2
+"Get the Party Started" - Pink
+"Lady Marmalade" - Christina Aguilera, Lil' Kim, Mya, Pink
+EOF
 cat songs.txt | grep "\- J" >>results.txt
-grep '\- J' songs.txt >> results.txt
