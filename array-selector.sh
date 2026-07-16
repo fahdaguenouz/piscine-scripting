46c65cfdc4b2336228a1741e1a3f1fe17b50c1e5
fahdaguenouz <faguenouz@gmail.com>
2026-07-16 06:46:24 -0400

    array

---
+++ array-selector.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+colors=("red" "blue" "green" "white" "black")
+
+if [[ $# -ne 1 ]]; then
+    echo "Error"
+elif [[ ! $1 =~ ^[0-9]+$ ]]; then
+    echo "Error"
+else
+    index=$1
+    if (( index < 1 || index > ${#colors[@]} )); then
+        echo "Error"
+    else
+        echo "${colors[index-1]}"
+    fi
+fi
