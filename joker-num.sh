d0dd3f77e7a9cef32a93160619b06676aabd2d03
fahdaguenouz <faguenouz@gmail.com>
2026-07-18 05:33:17 -0400

    joker

---
+++ joker-num.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check arguments
+if [ "$#" -ne 1 ] || ! [[ $1 =~ ^[0-9]+$ ]] || [ "$1" -lt 1 ] || [ "$1" -gt 100 ]; then
+    >&2 echo "Error: wrong argument"
+    exit 1
+fi
+
+secret=$1
+
+for ((tries=1; tries<=5; )); do
+    tries_left=$((6 - tries))
+
+    echo "Enter your guess ($tries_left tries left):"
+    read guess
+
+    # Invalid input: empty, non-numeric, or outside 1-100
+    if ! [[ $guess =~ ^[0-9]+$ ]] || [ "$guess" -lt 1 ] || [ "$guess" -gt 100 ]; then
+        continue
+    fi
+
+    if [ "$guess" -lt "$secret" ]; then
+        echo "Go up"
+    elif [ "$guess" -gt "$secret" ]; then
+        echo "Go down"
+    else
+        echo "Congratulations, you found the number in $tries moves!"
+        exit 0
+    fi
+
+    tries=$((tries + 1))
+done
+
+echo "You lost, the number was $secret"
